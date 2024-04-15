d76e76920566c3643b5263ffee3a0ee446a51bf6
ryuichi kondo <rkondo531@gmail.com>
2024-04-15 16:21:21 +0900

    building tailwindcss environment

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-orange-300">
           Hello, world!
         </p>
         <a
